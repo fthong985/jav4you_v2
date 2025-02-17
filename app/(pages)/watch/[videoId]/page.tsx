a2e9dc60ac0028ef084b523d7823360363295f98
kenneth <kennethdean08@gmail.com>
2025-02-18 06:05:56 +0800

    remove video metadata

--- app/(pages)/watch/[videoId]/page.tsx
+++ app/(pages)/watch/[videoId]/page.tsx
@@ -24,30 +24,30 @@ export type DescriptionTypes = {
 
 type GetVideoTypes = VideoTypes | { status: number; message: string };
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { videoId: string };
-}) {
-  const video: GetVideoTypes = await getVideo(params.videoId);
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { videoId: string };
+// }) {
+//   const video: GetVideoTypes = await getVideo(params.videoId);
 
-  const hasNoRes =
-    "status" in video && (video.status === 404 || video.status === 500);
+//   const hasNoRes =
+//     "status" in video && (video.status === 404 || video.status === 500);
 
-  const hasError = "status" in video;
+//   const hasError = "status" in video;
 
-  if (hasNoRes || hasError) return;
+//   if (hasNoRes || hasError) return;
 
-  return {
-    title: video.description.code,
-    description: video.synopsis,
-    openGraph: {
-      image: {
-        url: video.poster,
-      },
-    },
-  };
-}
+//   return {
+//     title: video.description.code,
+//     description: video.synopsis,
+//     openGraph: {
+//       image: {
+//         url: video.poster,
+//       },
+//     },
+//   };
+// }
 
 export default async function page({
   params,
