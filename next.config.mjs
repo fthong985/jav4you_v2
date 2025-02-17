a279ef5e548cd75e87f8fdf5b388a1985208dd74
kenneth <kennethdean08@gmail.com>
2025-02-18 00:55:09 +0800

    make import dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -14,19 +14,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*{/}?",
-        headers: [
-          {
-            key: "X-Accel-Buffering",
-            value: "no",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
