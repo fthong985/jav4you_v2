43b4707f6cd25ec38a580464553a5041aad2eb42
kenneth <kennethdean08@gmail.com>
2025-02-19 05:17:07 +0800

    unoptimized next image

--- components/NavsHeader.tsx
+++ components/NavsHeader.tsx
@@ -50,6 +50,7 @@ export default function NavsHeader() {
             className="absolute"
             sizes="(max-width: 300px) 100vw, (max-width: 450px) 50vw, 800px"
             priority
+            unoptimized
           />
         </div>
       </Link>
