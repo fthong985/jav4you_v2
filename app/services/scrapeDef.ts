27cf8f743cd0c83701702246d77e3462b0b13989
kenneth <kennethdean08@gmail.com>
2025-09-19 16:21:57 +0800

    commit 4

--- app/services/scrapeDef.ts
+++ app/services/scrapeDef.ts
@@ -281,7 +281,7 @@ export async function getGenre(endpoint: string) {
 
 export async function getActressList(endpoint: string = "") {
   try {
-    const url = `https://missav.ws/en/actresses${endpoint}`;
+    const url = `https://missav.live/en/actresses${endpoint}`;
 
     // Fetch HTML content using native fetch
     const response = await fetch(url, {
@@ -343,7 +343,7 @@ export async function getActressList(endpoint: string = "") {
 
 export async function getActressRanking() {
   try {
-    const url = `https://missav.ws/en/actresses/ranking`;
+    const url = `https://missav.live/en/actresses/ranking`;
 
     // Fetch HTML content using native fetch
     const response = await fetch(url, {
